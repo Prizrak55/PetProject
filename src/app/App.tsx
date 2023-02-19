b4eb07364e681c7aef0bcc67d0e4947c84650bab
Dmitry Morozov <kor502296@gmail.com>
2023-02-19 15:16:58 +0300

    ы

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,7 @@ function App() {
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
+        <Navbar />
         <div className="content-page">
           <Sidebar />
           <AppRouter />
